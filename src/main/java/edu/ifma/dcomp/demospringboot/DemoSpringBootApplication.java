1f8b31bf9a6e964521bda6be9fb8562c1b9e0a14
joaoslz <joaoslz@gmail.com>
2018-04-11 18:11:11 -0300

    Versão Inicial do projeto

---
+++ src/main/java/edu/ifma/dcomp/demospringboot/DemoSpringBootApplication.java
@@ -0,0 +1,14 @@
+package edu.ifma.dcomp.demospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("Carregando a nossa primeira aplicação");
+		SpringApplication.run(DemoSpringBootApplication.class, args);
+	}
+}
